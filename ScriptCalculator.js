05bbd61e51e13ce5740301a4dce0337708b489b6
MadisGrid <125402049+MadisGrid@users.noreply.github.com>
2023-03-06 16:03:02 -0600

    Update ScriptCalculator.js

--- ScriptCalculator.js
+++ ScriptCalculator.js
@@ -101,25 +101,54 @@ class Calculator {
     this.display.value = this.result.toString();
   }
 
-  start() {
-    // Create GridPane to place all buttons needed for the Calculator
-    const grid = document.createElement("div");
-    grid.style.padding = "10px";
-    grid.style.display = "grid";
-    grid.style.gridGap = "10px";
-    grid.style.gridTemplateColumns = "repeat(4, 50px)";
-    grid.style.gridTemplateRows = "repeat(4, 50px)";
-    grid.style.alignItems = "center";
-    grid.style.justifyContent = "center";
+start() {
+  // Create GridPane to place all buttons needed for the Calculator
+  const grid = document.createElement("div");
+  grid.style.padding = "10px";
+  grid.style.display = "grid";
+  grid.style.gridGap = "10px";
+  grid.style.gridTemplateColumns = "repeat(4, 50px)";
+  grid.style.gridTemplateRows = "repeat(5, 50px)";
+  grid.style.alignItems = "center";
+  grid.style.justifyContent = "center";
 
-    // Create buttons and add them to the grid
-    for (let i = 0; i < this.buttonLabels.length; i++) {
-      const button = document.createElement("button");
-      button.innerText = this.buttonLabels[i];
-      button.onclick = this.CalculatorButtonsHandler.handle;
-      this.buttons[i] = button;
-      grid.appendChild(button);
-    }
-    document.body.appendChild(grid);
-    document.body.appendChild(this.display);
+  // Create buttons and add them to the grid
+  for (let i = 0; i < this.buttonLabels.length; i++) {
+    const button = document.createElement("button");
+    button.innerText = this.buttonLabels[i];
+    button.style.width = "50px";
+    button.style.height = "50px";
+    button.style.fontSize = "20px";
+    button.style.borderRadius = "5px";
+    button.style.background = "#f2f2f2";
+    button.style.boxShadow = "1px 1px 1px rgba(0, 0, 0, 0.2)";
+    button.onclick = this.CalculatorButtonsHandler.handle;
+    this.buttons[i] = button;
+    grid.appendChild(button);
   }
+
+  // Add the last row of buttons
+  const clearButton = document.createElement("button");
+  clearButton.innerText = "C";
+  clearButton.style.width = "110px";
+  clearButton.style.height = "50px";
+  clearButton.style.fontSize = "20px";
+  clearButton.style.borderRadius = "5px";
+  clearButton.style.background = "#f2f2f2";
+  clearButton.style.boxShadow = "1px 1px 1px rgba(0, 0, 0, 0.2)";
+  clearButton.onclick = this.CalculatorButtonsHandler.handle;
+  const equalsButton = document.createElement("button");
+  equalsButton.innerText = "=";
+  equalsButton.style.width = "110px";
+  equalsButton.style.height = "50px";
+  equalsButton.style.fontSize = "20px";
+  equalsButton.style.borderRadius = "5px";
+  equalsButton.style.background = "#f2f2f2";
+  equalsButton.style.boxShadow = "1px 1px 1px rgba(0, 0, 0, 0.2)";
+  equalsButton.onclick = this.CalculatorButtonsHandler.handle;
+  grid.appendChild(clearButton);
+  grid.appendChild(equalsButton);
+
+  document.body.appendChild(grid);
+  document.body.appendChild(this.display);
+}
