e5a0f953843ae809d2d89a92c2291f300c3cdd4a
MadisGrid <125402049+MadisGrid@users.noreply.github.com>
2023-03-06 15:48:56 -0600

    Update ScriptCalculator.js

--- ScriptCalculator.js
+++ ScriptCalculator.js
@@ -116,36 +116,10 @@ class Calculator {
     for (let i = 0; i < this.buttonLabels.length; i++) {
       const button = document.createElement("button");
       button.innerText = this.buttonLabels[i];
-      button.onclick
-start() {
-    // Create GridPane to place all buttons needed for the Calculator
-    const grid = document.createElement('div');
-    grid.id = 'grid';
-    grid.style.padding = '10px';
-    grid.style.display = 'grid';
-    grid.style.gridGap = '10px';
-    grid.style.gridTemplateColumns = 'repeat(4, 50px)';
-    grid.style.gridTemplateRows = 'repeat(4, 50px)';
-    grid.style.alignItems = 'center';
-    grid.style.justifyContent = 'center';
-
-    // Create buttons and add them to the grid
-    for (let i = 0; i < this.buttonLabels.length; i++) {
-      const button = document.createElement('button');
-      button.innerText = this.buttonLabels[i];
-      button.onclick = (e) => this.CalculatorButtonsHandler.handle(e);
-      button.style.width = '50px';
-      button.style.height = '50px';
-      button.style.borderRadius = '50%';
-      button.style.backgroundColor = 'lightgray';
-      button.style.border = 'none'; // Added line
-      button.style.fontSize = '20px';
-      button.style.fontWeight = 'bold';
-      grid.appendChild(button);
+      button.onclick = this.CalculatorButtonsHandler.handle;
       this.buttons[i] = button;
+      grid.appendChild(button);
     }
-
-    // Add the grid element to the document
     document.body.appendChild(grid);
     document.body.appendChild(this.display);
   }
