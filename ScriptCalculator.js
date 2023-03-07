4d09c3d2056027849b3b38d3b20796f2ba282ef5
MadisGrid <125402049+MadisGrid@users.noreply.github.com>
2023-03-07 13:09:18 -0600

    Update ScriptCalculator.js

--- ScriptCalculator.js
+++ ScriptCalculator.js
@@ -1,87 +1,65 @@
 class Calculator {
   constructor() {
-    // Array to hold button label strings
-    this.buttonLabels = [
-      "7",
-      "8",
-      "9",
-      "/",
-      "4",
-      "5",
-      "6",
-      "*",
-      "1",
-      "2",
-      "3",
-      "-",
-      "0",
-      ".",
-      "=",
-      "+",
-      "C",
-    ];
-    // Array to hold button objects
-    this.buttons = new Array(this.buttonLabels.length);
-
-    // Variables to keep track of computation
     this.result = 0;
-    this.previousOperator = " ";
-    this.inputString = "";
+    this.currentOperator = null;
+    this.currentInput = null;
 
-    // Text field to display the number and result
-    this.display = document.createElement("input");
-    this.display.id = "display";
-    this.display.className = "display";
-    this.display.type = "text";
-    this.display.value = "0";
-    this.display.readOnly = true;
+    this.display = document.getElementById("display");
 
-    // CalculatorButtonsHandler Listener class to handle button clicks
-    const CalculatorButtonsHandler = {
-      handle: (e) => {
-        const buttonLabel = e.target.innerText;
-        switch (buttonLabel) {
-          case "C":
-            this.result = 0;
-            this.inputString = "";
-            this.previousOperator = " ";
-            this.display.value = "0";
-            break;
-          case "+":
-          case "-":
-          case "*":
-          case "/":
-            if (this.inputString !== "") {
-              this.compute();
-            }
-            this.previousOperator = buttonLabel.charAt(0);
-            break;
-          case "=":
-            if (this.inputString !== "") {
-              this.compute();
-              this.previousOperator = " ";
-            }
-            break;
-          default:
-            if (this.inputString === "0") {
-              this.inputString = buttonLabel;
-            } else {
-              this.inputString += buttonLabel;
-            }
-            this.display.value = this.inputString;
-            break;
-        }
-      },
-    };
-
-    this.CalculatorButtonsHandler = CalculatorButtonsHandler;
+    this.buttons = document.querySelectorAll(".button-grid input[type='button']");
+    this.buttons.forEach(button => {
+      const label = button.value;
+      button.addEventListener("click", () => this.handleButtonClick(label));
+    });
   }
 
-  // Method to handle calculator computations
+  handleButtonClick(label) {
+    switch (label) {
+      case "C":
+        this.clear();
+        break;
+      case "+":
+      case "-":
+      case "*":
+      case "/":
+        this.handleOperator(label);
+        break;
+      case "=":
+        this.compute();
+        break;
+      default:
+        this.handleDigit(label);
+        break;
+    }
+  }
+
+  clear() {
+    this.result = 0;
+    this.currentOperator = null;
+    this.currentInput = null;
+    this.display.value = "0";
+  }
+
+  handleOperator(operator) {
+    if (this.currentInput !== null) {
+      this.compute();
+    }
+    this.currentOperator = operator;
+    this.currentInput = null;
+  }
+
+  handleDigit(digit) {
+    if (this.currentInput === null) {
+      this.currentInput = digit;
+    } else {
+      this.currentInput += digit;
+    }
+    this.display.value = this.currentInput;
+  }
+
   compute() {
-    const inputNum = parseInt(this.inputString);
-    this.inputString = "";
-    switch (this.previousOperator) {
+    const inputNum = parseFloat(this.currentInput);
+    switch (this.currentOperator) {
       case "+":
         this.result += inputNum;
         break;
@@ -99,57 +77,9 @@ class Calculator {
         break;
     }
     this.display.value = this.result.toString();
+    this.currentOperator = null;
+    this.currentInput = null;
   }
-
-start() {
-  // Create GridPane to place all buttons needed for the Calculator
-  const grid = document.createElement("div");
-  grid.style.padding = "10px";
-  grid.style.display = "grid";
-  grid.style.gridGap = "10px";
-  grid.style.gridTemplateColumns = "repeat(4, 50px)";
-  grid.style.gridTemplateRows = "repeat(5, 50px)";
-  grid.style.alignItems = "center";
-  grid.style.justifyContent = "center";
-
-  // Create buttons and add them to the grid
-  for (let i = 0; i < this.buttonLabels.length; i++) {
-    const button = document.createElement("button");
-    button.innerText = this.buttonLabels[i];
-    button.style.width = "50px";
-    button.style.height = "50px";
-    button.style.fontSize = "20px";
-    button.style.borderRadius = "5px";
-    button.style.background = "#f2f2f2";
-    button.style.boxShadow = "1px 1px 1px rgba(0, 0, 0, 0.2)";
-    button.onclick = this.CalculatorButtonsHandler.handle;
-    this.buttons[i] = button;
-    grid.appendChild(button);
-  }
-
-  // Add the last row of buttons
-  const clearButton = document.createElement("button");
-  clearButton.innerText = "C";
-  clearButton.style.width = "110px";
-  clearButton.style.height = "50px";
-  clearButton.style.fontSize = "20px";
-  clearButton.style.borderRadius = "5px";
-  clearButton.style.background = "#f2f2f2";
-  clearButton.style.boxShadow = "1px 1px 1px rgba(0, 0, 0, 0.2)";
-  clearButton.onclick = this.CalculatorButtonsHandler.handle;
-  const equalsButton = document.createElement("button");
-  equalsButton.innerText = "=";
-  equalsButton.style.width = "110px";
-  equalsButton.style.height = "50px";
-  equalsButton.style.fontSize = "20px";
-  equalsButton.style.borderRadius = "5px";
-  equalsButton.style.background = "#f2f2f2";
-  equalsButton.style.boxShadow = "1px 1px 1px rgba(0, 0, 0, 0.2)";
-  equalsButton.onclick = this.CalculatorButtonsHandler.handle;
-  grid.appendChild(clearButton);
-  grid.appendChild(equalsButton);
-
-  document.body.appendChild(grid);
-  document.body.appendChild(this.display);
 }
 
+new Calculator();
